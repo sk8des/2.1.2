fab23ba1a52ed7c3714c6dc61a9aa1b24ad3a86d
sk8des <102260911+sk8des@users.noreply.github.com>
2022-04-08 16:30:32 +0300

    Firsr Commit

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(MysqlxDatatypes.Scalar.String[] args) {
+
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getName());
+
+        System.out.println(bean == bean2);
+        System.out.println(catBean == catBean2);
+    }
+}
